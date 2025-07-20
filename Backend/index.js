67f80f25b2fab7ddb4d11fff2c9dc336fae3bffa
Aman Chaurasiya <139747366+aman8906@users.noreply.github.com>
2025-07-20 08:08:56 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL.split(","),
+    origin: process.env.FRONTEND_URL(","),
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
